3e71e549ccd94e05ffa508cb7a9f83c9454aa7ba
demogth <96139364+demogth@users.noreply.github.com>
2024-09-03 00:36:33 +0300

    init

---
+++ roster.js
@@ -0,0 +1,16 @@
+const numbers = [12, 9, 31, 18, 22];
+const names = ['Joe Schmo', 'Ms. Buckets', 'Harvey Kay', 'Sally Talls', 'MK DiBoux'];
+const positions = ['Center', 'Point Guard', 'Shooting Guard', 'Power Forward', 'Small Forward'];
+const points = {
+  12: [14, 32, 7, 0, 23],
+  9: [19, 0, 11, 22, 0],
+  31: [0, 30, 16, 0, 25],
+  18: [18, 29, 26, 31, 19],
+  22: [11, 0, 23, 17, 0],
+};
+
+function generateRoster() {
+  // Код тут.
+}
+
+module.exports = { generateRoster };
---
+++ spec/roster.spec.js
@@ -0,0 +1,18 @@
+const { generateRoster } = require('../roster');
+
+describe('Board', function () {
+  // Тестов нет :\
+  // Попробуешь написать?
+
+  beforeEach(function () {
+    // board = .....
+  });
+
+  describe('place header', function () {
+    // Код тут.
+  });
+
+  describe('init board', function () {
+    // Код тут.
+  });
+});
